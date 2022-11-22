5ee97ac437b73a8cce45865e72d016f26050ae8b
Navjot Singh <navjotsingh@Navjots-MacBook-Air.local>
2022-11-21 21:33:43 -0500

    cors error fixed

--- src/main/java/com/emailsmtpspringboot/emailsmtprestspringboot/Email/EmailController.java
+++ src/main/java/com/emailsmtpspringboot/emailsmtprestspringboot/Email/EmailController.java
@@ -3,11 +3,13 @@ package com.emailsmtpspringboot.emailsmtprestspringboot.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class EmailController {
 
     @Autowired
